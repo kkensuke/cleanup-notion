a0cea2c8ed04d29ea21b344e5a2634aa8bd85752
kkensuke <n4659017382@gmail.com>
2023-07-26 14:55:55 +0900

    🔧 MAINTAIN:

--- README.md
+++ README.md
@@ -4,16 +4,17 @@
 `cleanup-notion.sh` does the following:
 - Removes UUID (Universally Unique Identifier), or space+32-digit alphanumeric characters, from filenames and directory names and their references in `*.md` and `*.csv` contents.
 - make `failed.txt` which contains the list of files and directories that failed to be renamed.
-- Replace `%20` with `_` in filenames and directory names and their references in `*.md` and `*.csv` contents. However, it may break URLs with `%20` and other strings with `%20`. So, at first, check with `find . -name "*.md" -exec grep "%20" {} +` and `find . -name "*.csv" -exec grep "%20" {} +`. If there is no `%20` except in the file or directory path, uncomment the following part and run the script.
+- Replace `%20` with `_` in filenames and directory names and their references in `*.md` and `*.csv` contents. However, it may break URLs with `%20` and other strings with `%20`. So, first, check with `find . -name "*.md" -exec grep "%20" {} +` and `find . -name "*.csv" -exec grep "%20" {} +`. If there is no `%20` except in the file or directory path, uncomment `replace_space` and run the script.
 
 	```bash
-	# 	echo "\n\n================================================="
-	# 	echo 'replace space in filenames with _'
-	# 	find . -depth -exec rename 's| |_|g' {} + 2>/dev/null
-	# 	echo "\n\n================================================="
-	# 	echo 'replace %20 in filenames with _ in file contents'
-	# 	find . -name "*.md" -exec sed -E -i "" 's|%20|_|g' {} +
-	# 	find . -name "*.csv" -exec sed -E -i "" 's|%20|_|g' {} +
+	main() {
+		echo "\033[0;36m"
+		rename_root
+		remove_UUID
+		# replace_space
+		others
+		notice
+	}
 	```
 
 
@@ -22,7 +23,7 @@
 
 
 ## Caution:
-- Please make a copy of your Exported directory before executing this just in case!
+- Please make a copy of your Exported directory before executing this script just in case!
 - You might have files or directories with the same name except UUID in the same directory, in that case, their links in `.md` files won't work after executing the script. I recommend that you rename those files before exporting from notion.
 
 
--- cleanup-notion.sh
+++ cleanup-notion.sh
@@ -6,20 +6,20 @@ set -u
 
 
 
-main() {
-	echo "\033[0;32m"
-
+rename_root(){
 	for item in $(ls)
 	do
 		if [[ $item =~ Export-.* ]]; then
 			mv Export-*/ Export/
 		fi
 	done
+}
 
+
+remove_UUID(){
 	echo "================================================="
 	echo 'remove UUIDs in filenames and dirnames'
 	find . -depth -exec rename 's|(.*) [a-z0-9]{32}|\1|' {} + 2>/dev/null
-
 
 	echo "\n\n================================================="
 	echo 'remove UUIDs in filenames in .md contents'
@@ -30,38 +30,58 @@ main() {
 	find . -name "*.csv" -exec sed -i "" 's|%20[a-z0-9]\{32\}\.csv|\.csv|g' {} +
 	find . -name "*.csv" -exec sed -i "" 's|%20[a-z0-9]\{32\}\.md|\.md|g' {} +
 	# find . -name "*_all.csv" -type f -delete
+}
 
 
-# 	echo "\n\n================================================="
-# 	echo 'replace space in filenames with _'
-# 	find . -depth -exec rename 's| |_|g' {} + 2>/dev/null
-# 	echo "\n\n================================================="
-# 	echo 'replace %20 in filenames with _ in file contents'
-# 	find . -name "*.md" -exec sed -E -i "" 's|%20|_|g' {} +
-# 	find . -name "*.csv" -exec sed -E -i "" 's|%20|_|g' {} +
-
+replace_space(){
+	echo "\n\n================================================="
+	echo 'replace space in filenames with _'
+	find . -depth -exec rename 's| |_|g' {} + 2>/dev/null
 
 	echo "\n\n================================================="
-	echo "Done!\n"
-	ls -R | grep " [a-z0-9]\{32\}" >> failed.txt
-	echo "Check files failed to rename: cat failed.txt"
+	echo 'replace %20 in filenames with _ in file contents'
+	find . -name "*.md" -exec sed -E -i "" 's|%20|_|g' {} +
+	find . -name "*.csv" -exec sed -E -i "" 's|%20|_|g' {} +
+}
+
+
+notice(){
+	echo "\n\n================================================="
+	echo "Done"
+	ls -R | grep "[a-z0-9]\{32\}" > failed.txt
+	echo "\n\033[0;44mCheck files failed to be renamed: cat failed.txt"
+}
+
+
+main() {
+	echo "\033[0;36m"
+	rename_root
+	remove_UUID
+# 	replace_space
+	notice
+}
+
+
+caution(){
+	echo "\033[0;33m"
+	echo "====================================================================================="
+	echo "\nCaution: Please make a copy of your Exported directory before executing this script just in case"
+	read -p "Press enter to continue;"
+	echo "====================================================================================="
+	echo "\nCaution: You might have files or directories with the same name except UUID in the same directory, in that case, their links in .md files won't work after executing the script.\n"
+	echo "I recommend that you rename those files before exporting from notion.\n"
+}
+
+
+ready(){
+	read -r -p "Are you ready? [y/N] " response
+	case "$response" in
+	    [yY][eE][sS]|[yY]) $1 ;;
+	                    *) echo "\033[0;41m"; echo "Stop" ;;
+	esac
 }
 
 
 
-echo "\033[0;33m"
-echo "====================================================================================="
-echo "\nCaution: Please make a copy of your Exported directory before executing this script just in case!"
-read -p "Press enter to continue;"
-echo "====================================================================================="
-echo "\nCaution: You might have files or directories with the same name except UUID in the same directory, in that case, their links in .md files won't work after executing the script.\n"
-read -p "Press enter to continue;"
-echo "====================================================================================="
-echo "\nI recommend that you rename those files before exporting from notion.\n"
-
-read -r -p "Are you ready? [y/N] " response
-case "$response" in
-    [yY][eE][sS]|[yY]) main ;;
-                    *) echo "\033[0;41m"; echo "Stop!" ;;
-esac
-
+caution
+ready main
